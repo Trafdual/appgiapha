ad8e9f1285da35bf264b6283e1f52f076e4b07fc
trafdual <traz08102003@gmail.com>
2024-01-08 22:38:12 +0700

    f

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const userroute=require('./routes/UserRoutes');
+
+var app = express();
+app.use(methodOverride('_method'));
+
+const uri = "mongodb+srv://traz08102003:G1XMVWTucFqfpNch@cp17303.4gzmzyt.mongodb.net/giapha?retryWrites=true&w=majority";
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(console.log("kết nối thành công"));
+
+const mongoStoreOptions = {
+  mongooseConnection: mongoose.connection,
+  mongoUrl: uri,
+  collection: 'sessions', 
+};
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'mysecretkey',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create(mongoStoreOptions),
+  cookie: {
+    secure: false,
+  }
+}));
+app.use(cors());
+ 
+app.use('/',userroute);
+
+app.listen(8080, () => {
+  try {
+    console.log('kết nối thành công 8080')
+  } catch (error) {
+    console.log('kết nối thất bại 8080', error)
+  }
+}
+);
---
+++ models/UserModels.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  success:{type:Boolean,enum:[true], default:true},
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  hovaten:{type:String},
+  date:{type:String},
+  yearsold:{type:String},
+  address:{
+    wards:{type:String},
+    districts:{type:String},
+    city:{type:String}
+  },
+  hometown:{
+    wards:{type:String},
+    districts:{type:String},
+    city:{type:String}
+  },
+  avatar:{type:String},
+  phone:{type:String},
+  job:{type:String}
+});
+
+const User = mongoose.model('user', userSchema);
+module.exports = User;
