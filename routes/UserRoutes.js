252cf1799845e8327008f841b1a77e775898e276
trafdual <traz08102003@gmail.com>
2024-01-08 22:39:24 +0700

    f

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -6,7 +6,7 @@ const multer = require('multer')
 
 const storage = multer.memoryStorage();
 
-const upload = multer({ storage: storage });
+
 
 router.post('/register', async (req, res) => {
     try {
