ca05989a6b9b95fb50b2594815972aab73c1f14c
ducbinhnguyennd <ducbinhnguyennd@gmail.com>
2024-02-27 17:25:01 +0700

    update code

--- models/QueBoiModels.js
+++ models/QueBoiModels.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const queboiSchema = new mongoose.Schema({
-nameque:{type:Number},
+nameque:{type:String},
 content:{type:String},
 idxam:{type: mongoose.Schema.Types.ObjectId, ref: 'xam'}
 });
