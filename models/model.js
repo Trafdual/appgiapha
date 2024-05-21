076467abacb7701306d4a7a07efe014d89cee028
trafdual <traz08102003@gmail.com>
2024-05-22 01:28:11 +0700

    f

--- models/model.js
+++ models/model.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 
 const modelSchema = new mongoose.Schema({
 name:{type:String},
+color:{type:String},
+imel:{type:String},
+dungluong:{type:String}
 });
 
 const Model = mongoose.model('model', modelSchema);
--- routes/ModelRouter.js
+++ routes/ModelRouter.js
@@ -4,11 +4,8 @@ const multer = require("multer");
 const { google } = require('googleapis');
 const Model = require("../models/model");
 const { Readable } = require('stream');
-const puppeteer = require('puppeteer');
-const key=require('../trusty-magnet-424007-d8-5c8a29b5b083.json')
 // const fs=require('fs')
-const path=require('path')
-const axios = require('axios');
+const path = require('path')
 // const cheerio = require('cheerio');
 // const mongoose = require('mongoose');
 // const mongoURI= "mongodb+srv://traz08102003:G1XMVWTucFqfpNch@cp17303.4gzmzyt.mongodb.net/giapha?retryWrites=true&w=majority"
@@ -25,8 +22,12 @@ const axios = require('axios');
 // });
 
 
-const SCOPES = ['https://www.googleapis.com/auth/drive.readonly'];
-const KEYFILEPATH = path.resolve(__dirname, '../trusty-magnet-424007-d8-5c8a29b5b083.json'); // Thay bằng đường dẫn thực tế đến tệp credentials.json
+const SCOPES = 
+['https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/drive.file', 
+  'https://www.googleapis.com/auth/drive.readonly'];
+
+  const KEYFILEPATH = path.resolve(__dirname, '../trusty-magnet-424007-d8-5c8a29b5b083.json'); // Thay bằng đường dẫn thực tế đến tệp credentials.json
 async function authenticate() {
   const auth = new google.auth.GoogleAuth({
     keyFile: KEYFILEPATH,
@@ -77,91 +78,28 @@ router.post('/readfiles', async (req, res) => {
     const auth = await authenticate();
     const files = await listFilesInFolder(auth, folderId);
 
-    const fileContents = [];
-    for (const file of files) {
-      const content = await downloadFile(auth, file.id);
-      fileContents.push({ fileName: file.name, content });
-    }
+    files.sort((a, b) => new Date(b.createdTime) - new Date(a.createdTime));
 
-    res.json(fileContents);
+    const latestFile = files[0];
+    
+    const content = await downloadFile(auth, latestFile.id);
+    const nameMatch=content.match(/name:(.*)/);
+    const imelmatch = content.match(/imel:(.*)/);
+    const colorMatch=content.match(/color:(.*)/);
+    const dungluongmatch = content.match(/dungluong:(.*)/);
+    const name = nameMatch[1].replace(/\\/g, '').trim();
+    const color=colorMatch[1].replace(/\\/g, '').trim();
+    const imel=imelmatch[1].replace(/\\/g, '').trim();
+    const dungluong=dungluongmatch[1].replace(/\\/g, '').trim();
+    const model= new Model({name, color, imel, dungluong});
+    await model.save();
+    res.redirect('/hi');
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Đã xảy ra lỗi khi đọc các tệp.', error: error.message });
   }
 });
 
-
-router.post('/modelpost1', upload.array('files'), async (req, res) => {
-  try {
-    let uploadedFiles = req.files;
-
-    if (!uploadedFiles || uploadedFiles.length === 0) {
-      return res.status(400).json({ message: 'Không có tệp nào được tải lên.' });
-    }
-
-    const newModels = [];
-    const uniqueFiles = []; // Mảng lưu các file không trùng nội dung
-
-    for (const uploadedFile of uploadedFiles) {
-      const readableStream = Readable.from(uploadedFile.buffer);
-      let fileContent = '';
-
-      readableStream.on('data', (chunk) => {
-        fileContent += chunk.toString();
-      });
-
-      await new Promise((resolve, reject) => {
-        readableStream.on('end', async () => {
-          const fileContent = uploadedFile.buffer.toString('utf8');
-          const nameMatch = fileContent.match(/name=(.*)/);
-          if (nameMatch) {
-            const name = nameMatch[1].replace(/\\/g, '').trim();
-            const duplicate = await Model.findOne({ name });
-            if (!duplicate) {
-              uniqueFiles.push(uploadedFile);
-            }
-          }
-          resolve();
-        });
-
-        readableStream.on('error', reject);
-      });
-    }
-
-    // Upload unique files to Google Drive
-    for (const uniqueFile of uniqueFiles) {
-      const readableStream = Readable.from(uniqueFile.buffer);
-
-      const driveResponse = await drive.files.create({
-        requestBody: {
-          name: uniqueFile.originalname,
-          mimeType: uniqueFile.mimetype,
-        },
-        media: {
-          mimeType: uniqueFile.mimetype,
-          body: readableStream,
-        },
-      });
-
-      const fileContent = uniqueFile.buffer.toString('utf8');
-      const nameMatch = fileContent.match(/name=(.*)/);
-
-      if (!nameMatch) {
-        return res.status(400).json({ message: 'Tệp tin không chứa trường name hợp lệ.' });
-      }
-
-      const name = nameMatch[1].replace(/\\/g, '').trim();
-      const newModel = new Model({ name, fileId: driveResponse.data.id }); // Lưu cả fileId từ Google Drive
-      await newModel.save();
-      newModels.push(newModel);
-    }
-
-    res.json(newModels);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Đã xảy ra lỗi khi thêm giấc mộng mới.' });
-  }
-});
 
 router.get("/model", async (req, res) => {
   try {
@@ -200,6 +138,12 @@ router.post("/deletemodel/:id", async (req, res) => {
   }
 });
 router.get("/hi", async (req, res) => {
-  res.render("hi");
+  try {
+    const model= await Model.find().lean();
+    res.render("hi",{model});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Đã xảy ra lỗi khi thêm giấc mộng mới." });
+  }
 });
 module.exports = router;
